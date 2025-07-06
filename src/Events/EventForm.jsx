ef40c625a5940edea163960206ce54825632f47c
MariaKukucheva <133159997+MariaKukucheva@users.noreply.github.com>
2025-07-06 17:18:40 +0300

    Add validation for date and time

--- src/Events/EventForm.jsx
+++ src/Events/EventForm.jsx
@@ -7,15 +7,27 @@ import { MobileTimePicker } from "@mui/x-date-pickers";
 
 function EventForm({ onSubmit }) {
   const [event, setEvent] = useState('');
-  const [error, setError] = useState(false);
   const [date, setDate] = useState(null);
   const [time, setTime] = useState(null);
+
+  const [error, setError] = useState({
+    name: false,
+    date: false,
+    time: false,
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (event.trim() === '') {
-      setError(true);
+    const newError = {
+      name: event.trim() === "",
+      date: date === null,
+      time: time === null,
+    };
+
+    setError(newError);
+
+     if (Object.values(newError).some(Boolean)) {  //ако има дори една грешка да не събмитва
       return;
     }
 
@@ -24,7 +36,11 @@ function EventForm({ onSubmit }) {
     setEvent('');
     setDate(null);
     setTime(null);
-    setError(false);
+    setError({
+      name: false,
+      date: false,
+      time: false,
+    });
   };
 
   return (
@@ -33,10 +49,10 @@ function EventForm({ onSubmit }) {
         <TextField
           value={event}
           onChange={(e) => setEvent(e.target.value)}
-          helperText={error ? 'Please enter an event' : ''}
+          helperText={error.name ? 'Please enter an event' : ''}
           label="Add event"
           color="secondary"
-          error={error}
+          error={error.name}
           sx={{ width: 250 }}
         />
 
@@ -44,16 +60,26 @@ function EventForm({ onSubmit }) {
           label="Select date"
           value={date}
           onChange={(newValue) => setDate(newValue)}
-          sx={{ width: 250 }}
+          slotProps={{
+          textField: {
+            error: error.date,
+            helperText: error.date ? "Please enter a date" : "",
+            sx: { width: 250 }
+          }
+        }}
         />
 
-        <DemoItem >
-          <MobileTimePicker value={time}
-            onChange={(newValue) => setTime(newValue)}
-             slotProps={{ textField: { sx: { width: 250 }}}}
-          />
-        </DemoItem>
-
+        <MobileTimePicker
+          value={time}
+          onChange={(newValue) => setTime(newValue)}
+          slotProps={{
+            textField: {
+              error: error.time,
+              helperText: error.time ? "Please enter a time" : "",
+              sx: { width: 250 }
+            }
+          }}
+        />
         <IconButton
           type="submit"
           aria-label="add"
